5faf3b9cc41ab344b95b1077a4722043f75e876f
PivrikArtem <artempivrik90@gmail.com>
2019-01-15 15:00:07 +0200

    Updates

---
+++ precache-manifest.2c8093606b8711992306d89663f2c7df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89a3c9c49ed1cc71b749",
+    "url": "/my-resume/static/css/main.89fcbf9a.chunk.css"
+  },
+  {
+    "revision": "89a3c9c49ed1cc71b749",
+    "url": "/my-resume/static/js/main.89a3c9c4.chunk.js"
+  },
+  {
+    "revision": "27ff1eab20b47ebc1c7e",
+    "url": "/my-resume/static/js/1.27ff1eab.chunk.js"
+  },
+  {
+    "revision": "54fe26f0c54799edc228",
+    "url": "/my-resume/static/js/runtime~main.54fe26f0.js"
+  },
+  {
+    "revision": "b5006750d3a1fb30d6e95b3f8305bf61",
+    "url": "/my-resume/static/media/modernBuild.b5006750.png"
+  },
+  {
+    "revision": "3cd4b4b3aff5e6400f5cb50a31958d75",
+    "url": "/my-resume/index.html"
+  }
+];
